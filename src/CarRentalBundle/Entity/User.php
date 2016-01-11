3876a7fd6587aca171c9a18804eb8ca89dc7e6f0
Patryk Kalinowski <patryk.kalinowski@netguru.pl>
2016-01-11 20:01:23 +0100

    FOSUser configured

---
+++ src/CarRentalBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CarRentalBundle\Entity;
+
+/**
+ * User
+ */
+class User
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
