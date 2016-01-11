591025b163f8930e63ee15d71804060983b6f6e4
Patryk Kalinowski <patryk.kalinowski@netguru.pl>
2016-01-11 18:27:17 +0100

    Creating and showing car

--- src/CarRentalBundle/Controller/DefaultController.php
+++ src/CarRentalBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace CarRentalBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CarRentalBundle\Entity\Car;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CarRentalBundle:Default:index.html.twig');
+        // create new car
+        // $car = new Car();
+        // $car -> setName('Ford Mustang');
+        // $car -> setType('muscle car');
+        // $car -> setHorsepower(300);
+        // $car -> setColor('red');
+        // $car -> setPhoto('none');
+        //
+        // $em = $this->getDoctrine()->getManager();
+        //
+        // $em->persist($car);
+        // $em->flush();
+
+        // return new Response("Created new car with id ".$car->getID()." Name: ".$car->getName());
+        return $this->render('CarRentalBundle:Default:index.html.twig', array('car' => $car));
     }
 }
